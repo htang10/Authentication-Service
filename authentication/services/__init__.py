0212e2e6e77dbe216f2c21f33d0375a49c6b5083
Huy Tang <htang.prof@outlook.com>
2026-04-14 19:03:37 -0700

    refactor: exclude modules that do not have contracts with other apps

--- authentication/services/__init__.py
+++ authentication/services/__init__.py
@@ -1,2 +1,12 @@
-from .mailing import verify_token, handle_email_verification
-from .user import get_user_by_refresh_token, update_user_login_metadata, update_user_logout_metadata
+from .mailing.verification import (
+    send_email_change_link,
+    send_email_verification_link,
+    send_password_reset_link,
+)
+from .tokens.verification import verify_token
+from .user import (
+    create_user,
+    get_user_by_refresh_token,
+    update_user_login_metadata,
+    update_user_logout_metadata,
+)
