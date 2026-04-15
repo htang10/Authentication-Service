7de0f181474537c1d88875f0ad6f155295b608b8
Huy Tang <htang.prof@outlook.com>
2026-04-14 18:56:08 -0700

    refactor: add type hint, user creation function and abandon reeturning user object in metadata updates

--- authentication/services/user.py
+++ authentication/services/user.py
@@ -1,39 +1,48 @@
 from django.utils import timezone
 from rest_framework_simplejwt.tokens import RefreshToken
 
-from authentication.models import User
-from authentication.utils import get_client_ip
+from authentication.models import Token, User
 
 
-def get_user_by_refresh_token(token):
+def create_user(*, email: str, password: str, **extra_fields) -> User:
+    user = User.objects.create(email=email, **extra_fields)
+    user.set_password(password)
+    user.save()
+    return user
+
+
+def get_user_by_refresh_token(token: Token) -> User:
     """Retrieves a user by refresh token"""
     user_id = RefreshToken(token)["user_id"]
     user = User.objects.get(pk=user_id)
     return user
 
 
-def update_user_login_metadata(user, request):
+def update_user_login_metadata(user: User, ip_address: str, user_agent: str) -> None:
     """
     Updates the user login metadata including timestamp, ip address, user agent and medium.
     """
     now = timezone.now()
     user.last_login_time = now
     user.last_active = now
-    user.last_login_ip = get_client_ip(request)
-    user.last_login_uagent = request.META.get("HTTP_USER_AGENT", "")
+    user.last_login_ip = ip_address
+    user.last_login_uagent = user_agent
     user.last_login_medium = "email"
-    user.save(update_fields=[
-        "last_login_time", "last_active", "last_login_ip",
-        "last_login_uagent", "last_login_medium"
-    ])
-    return user
+    user.save(
+        update_fields=[
+            "last_login_time",
+            "last_active",
+            "last_login_ip",
+            "last_login_uagent",
+            "last_login_medium",
+        ]
+    )
 
 
-def update_user_logout_metadata(user, request):
+def update_user_logout_metadata(user: User, ip_address: str) -> None:
     """
     Updates the user logout metadata including timestamp and ip address
     """
     user.last_logout_time = timezone.now()
-    user.last_logout_ip = get_client_ip(request)
+    user.last_logout_ip = ip_address
     user.save(update_fields=["last_logout_time", "last_logout_ip"])
-    return user
