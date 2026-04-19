93ebbcabfd73b13eaf4e366c4b71faa9430fad3a
Huy Tang <htang.prof@outlook.com>
2026-04-18 19:46:54 -0700

    feat: add service module to update password

--- authentication/services/__init__.py
+++ authentication/services/__init__.py
@@ -3,10 +3,13 @@ from .mailing.verification import (
     send_email_verification_link,
     send_password_reset_link,
 )
-from .tokens.verification import verify_token
+from .password import reset_password
+from .tokens.ott import verify_token
 from .user import (
     create_user,
+    find_existing_user,
     get_user_by_refresh_token,
+    mark_user_verified,
     update_user_login_metadata,
     update_user_logout_metadata,
 )
