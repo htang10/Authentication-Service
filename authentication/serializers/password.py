5755e85f451243ad632d6f644d452979378df26e
Huy Tang <htang.prof@outlook.com>
2026-04-18 17:50:31 -0700

    fix: enforce case-insensitive constraint in email validation and disable the validator from model

--- authentication/serializers/password.py
+++ authentication/serializers/password.py
@@ -1,43 +1,45 @@
+from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
 from rest_framework.authentication import authenticate
 
-from authentication.exceptions import EmailNotVerified, InvalidCredentials
+from authentication.exceptions import (
+    EmailAlreadyExists,
+    EmailNotVerified,
+    InvalidCredentials,
+)
 from authentication.models import User
-from authentication.services import create_user
+from authentication.services import create_user, find_existing_user
 from authentication.utils import normalize_email
 
 
 class SignUpSerializer(serializers.ModelSerializer):
-    """
-    Serializer for sign-up
-
-    Creates new user account if email has not been registered yet
-    Hashes password before storing user in db
-    """
-
     class Meta:
         model = User
         fields = ["email", "password", "display_name", "created_at", "updated_at"]
         read_only_fields = ["display_name", "created_at", "updated_at"]
         extra_kwargs = {
+            "email": {"validators": []},
             "password": {
-                "min_length": 8,
                 "write_only": True,
                 "style": {"input_type": "password"},
-            }
+            },
         }
+
+    def validate(self, data):
+        email = normalize_email(data["email"])
+
+        user = find_existing_user(email)
+        if user:
+            raise EmailAlreadyExists
+
+        validate_password(data["password"])
+        return data
 
     def create(self, validated_data):
         return create_user(**validated_data)
 
 
 class LoginSerializer(serializers.Serializer):
-    """
-    Serializer for login
-
-    Checks for existing verified email and correct password
-    """
-
     email = serializers.EmailField(max_length=255)
     password = serializers.CharField(write_only=True, style={"input_type": "password"})
 
