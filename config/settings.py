1ddd61c18a75b15164a2c1856c2837a6941dace1
Huy Tang <htang.prof@outlook.com>
2026-04-18 18:11:28 -0700

    refactor: enforce password length in settings for reusability

--- config/settings.py
+++ config/settings.py
@@ -172,6 +172,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+        "OPTIONS": {"min_length": 8},
     },
     {
         "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
