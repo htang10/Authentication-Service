f68aa2f6990dca9bed2c919db8b370860bc1362b
Huy Tang <htang.prof@outlook.com>
2026-04-18 19:47:26 -0700

    feat: add urls for password reset flow

--- authentication/urls.py
+++ authentication/urls.py
@@ -2,19 +2,30 @@ from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
 
 from authentication.views import (
-    SignUpEndpoint,
+    ForgotPasswordEndpoint,
     LoginEndpoint,
     LogoutEndpoint,
-    VerifyEmailEndpoint,
     ResendVerificationEndpoint,
+    ResetPasswordEndpoint,
+    SignUpEndpoint,
+    VerifyTokenEndpoint,
 )
 
-
 urlpatterns = [
+    # Email/password
     path("signup/", SignUpEndpoint.as_view(), name="signup"),
     path("login/", LoginEndpoint.as_view(), name="login"),
+    path("verify/", VerifyTokenEndpoint.as_view(), name="verify"),
+    path(
+        "resend-verification/",
+        ResendVerificationEndpoint.as_view(),
+        name="resend-verification",
+    ),
+    # Logout
     path("logout/", LogoutEndpoint.as_view(), name="logout"),
-    path("verify/", VerifyEmailEndpoint.as_view(), name="verify"),
-    path("resend-verification/", ResendVerificationEndpoint.as_view(), name="resend_verification"),
+    # Password management
+    path("forgot-password/", ForgotPasswordEndpoint.as_view(), name="forgot-password"),
+    path("reset-password/", ResetPasswordEndpoint.as_view(), name="reset-password"),
+    # Auth token
     path("get-access-token/", TokenRefreshView.as_view(), name="get-access-token"),
 ]
