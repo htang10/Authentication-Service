32832936719bc10152d6ac52222dc76e33160ebe
Huy Tang <htang.prof@outlook.com>
2026-04-22 10:52:38 -0700

    fix: add check for other types of exceptions that inherit from APIException with different response format

--- utils/exception_handler.py
+++ utils/exception_handler.py
@@ -4,6 +4,10 @@ from rest_framework.views import exception_handler
 def custom_exception_handler(exc, context):
     response = exception_handler(exc, context)
 
-    if response is not None and "detail" in response.data:
-        response.data = {"error": response.data["detail"]}
+    if response is not None:
+        if "detail" in response.data:
+            response.data = {"error": response.data["detail"]}
+        elif isinstance(response.data, list):
+            response.data = {"error": response.data[0]}
+
     return response
