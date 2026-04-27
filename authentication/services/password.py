54e121008a86d579e2f513e2c8a0dfe5746288a4
Huy Tang <htang.prof@outlook.com>
2026-04-27 14:45:23 -0700

    refactor: update shared token hashing module

--- authentication/services/password.py
+++ authentication/services/password.py
@@ -1,9 +1,9 @@
 from authentication.models import Token
-from authentication.services.tokens.ott import hash_token, mark_used_token
+from authentication.services.tokens import hash_secret, mark_used_token
 
 
 def reset_password(token_str: str, new_password: str) -> None:
-    hashed_token = hash_token(token_str)
+    hashed_token = hash_secret(token_str)
     token = Token.objects.get(token=hashed_token)
     user = token.user
 
