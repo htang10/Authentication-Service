76581a4bf5116f770fa63c84bc13da841644fc4e
Huy Tang <htang.prof@outlook.com>
2026-04-27 14:50:43 -0700

    fix: ensure that exception handler can handle all possible exception formats

--- utils/exception_handler.py
+++ utils/exception_handler.py
@@ -4,10 +4,12 @@ from rest_framework.views import exception_handler
 def custom_exception_handler(exc, context):
     response = exception_handler(exc, context)
 
-    if response is not None:
+    if response.data is not None:
         if "detail" in response.data:
-            response.data = {"error": response.data["detail"]}
+            response.data = {"error": response.data.get("detail")}
         elif isinstance(response.data, list):
             response.data = {"error": response.data[0]}
+        else:
+            response.data = {"error": response.data.get("non_field_errors")[0]}
 
     return response
