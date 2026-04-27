3a6ad1bf1651b1a6fc717a0623f9cdf954a0b8e1
Huy Tang <htang.prof@outlook.com>
2026-04-27 14:56:02 -0700

    feat: add serializers for otp generation and sign-up/login

--- authentication/serializers/__init__.py
+++ authentication/serializers/__init__.py
@@ -1,4 +1,5 @@
 from .logout import LogoutSerializer
+from .otp import CodeGenerateSerializer, CodeLoginSerializer
 from .password import LoginSerializer, SignUpSerializer
 from .password_management import ForgotPasswordSerializer, ResetPasswordSerializer
 from .verification import ResendVerificationSerializer
