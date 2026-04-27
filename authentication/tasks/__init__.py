31948a9f010448c411e737457f87250df7d15e46
Huy Tang <htang.prof@outlook.com>
2026-04-27 15:06:45 -0700

    feat: add otp email sending task

--- authentication/tasks/__init__.py
+++ authentication/tasks/__init__.py
@@ -1,5 +1,6 @@
 from .emails import (
     send_email_change_link_task,
     send_email_verification_link_task,
+    send_otp_email_task,
     send_password_reset_link_task,
 )
--- authentication/tasks/emails.py
+++ authentication/tasks/emails.py
@@ -1,26 +1,32 @@
 from celery import shared_task
 
 from authentication.models import User
-from authentication.services.mailing.verification import (
+from authentication.services.mailing import (
     send_email_change_link,
     send_email_verification_link,
+    send_otp_email,
     send_password_reset_link,
 )
 
 
 @shared_task
-def send_email_verification_link_task(user_id: int) -> None:
-    user = User.objects.get(id=user_id)
+def send_email_verification_link_task(email: str) -> None:
+    user = User.objects.get(email=email)
     send_email_verification_link(user)
 
 
 @shared_task
-def send_password_reset_link_task(user_id: int) -> None:
-    user = User.objects.get(id=user_id)
+def send_password_reset_link_task(email: str) -> None:
+    user = User.objects.get(email=email)
     send_password_reset_link(user)
 
 
 @shared_task
-def send_email_change_link_task(user_id: int) -> None:
-    user = User.objects.get(id=user_id)
+def send_email_change_link_task(email: str) -> None:
+    user = User.objects.get(email=email)
     send_email_change_link(user)
+
+
+@shared_task
+def send_otp_email_task(email: str) -> None:
+    send_otp_email(email)
