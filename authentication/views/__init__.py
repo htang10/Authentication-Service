68111824950e3b769e683c142f5715469655e3fc
Huy Tang <htang.prof@outlook.com>
2026-04-27 14:56:46 -0700

    feat: add endpoints for otp generation and sign-up/login

--- authentication/views/__init__.py
+++ authentication/views/__init__.py
@@ -1,4 +1,5 @@
 from .logout import LogoutEndpoint
+from .otp import CodeGenerateEndpoint, CodeLoginEndpoint
 from .password import LoginEndpoint, SignUpEndpoint
 from .password_management import ForgotPasswordEndpoint, ResetPasswordEndpoint
 from .verification import ResendVerificationEndpoint, VerifyTokenEndpoint
