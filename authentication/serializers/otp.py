7bc6463fc8ecc363c495c70d2bff04eccd767541
Huy Tang <htang.prof@outlook.com>
2026-04-27 14:56:02 -0700

    feat: add serializers for otp generation and sign-up/login

---
+++ authentication/serializers/otp.py
@@ -0,0 +1,32 @@
+from rest_framework import serializers
+
+from authentication.services import create_user, find_existing_user, verify_code
+from authentication.utils import normalize_email
+
+
+class CodeGenerateSerializer(serializers.Serializer):
+    email = serializers.EmailField(max_length=255)
+
+    def validate(self, data):
+        data["email"] = normalize_email(data["email"])
+
+        return data
+
+
+class CodeLoginSerializer(serializers.Serializer):
+    email = serializers.EmailField(max_length=255)
+    code = serializers.CharField(max_length=6)
+
+    def validate(self, data):
+        email = normalize_email(data["email"])
+        code = data["code"]
+
+        verify_code(code, email)
+
+        return {"email": email}
+
+    def create(self, validated_data):
+        user = find_existing_user(validated_data["email"])
+        if not user:
+            return create_user(**validated_data)
+        return user
