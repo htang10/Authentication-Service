ff2392221848e0c1c4b1c7f014987dd642a29027
Huy Tang <htang.prof@outlook.com>
2026-04-30 16:26:28 -0700

    refactor: fix typo for non_field_errors and reformat other error formats

--- utils/exception_handler.py
+++ utils/exception_handler.py
@@ -4,12 +4,18 @@ from rest_framework.views import exception_handler
 def custom_exception_handler(exc, context):
     response = exception_handler(exc, context)
 
-    if response.data is not None:
-        if "detail" in response.data:
-            response.data = {"error": response.data.get("detail")}
-        elif isinstance(response.data, list):
-            response.data = {"error": response.data[0]}
-        else:
-            response.data = {"error": response.data.get("non_field_errors")[0]}
+    if response is None:
+        return None
+
+    data = response.data
+
+    if "detail" in data:
+        response.data = {"error": data["detail"]}
+    elif "non_field_errors" in data:
+        response.data = {"error": data["non_field_errors"][0]}
+    elif isinstance(data, list):
+        response.data = {"error": data[0]}
+    else:
+        response.data = {"error": {field: errors[0] for field, errors in data.items()}}
 
     return response
