862d1fad514b6b5663fb7043947f37765722e9f2
Huy Tang <htang.prof@outlook.com>
2026-04-30 17:21:54 -0700

    docs: remove redundant docstring

--- authentication/serializers/logout.py
+++ authentication/serializers/logout.py
@@ -2,10 +2,4 @@ from rest_framework import serializers
 
 
 class LogoutSerializer(serializers.Serializer):
-    """
-    Serializer for logout
-
-    Blacklists refresh token after logout
-    """
-
     refresh = serializers.CharField()
