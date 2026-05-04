e1d7254460ac11053f67a6a6b55aacc9cb9db08b
Huy Tang <htang.prof@outlook.com>
2026-05-04 09:44:24 -0700

    refactor: fix type hint of generate_otp and adopt context manager to handle mailing errors more efficiently

--- authentication/services/mailing/verification_otp.py
+++ authentication/services/mailing/verification_otp.py
@@ -1,21 +1,12 @@
-import logging
-from smtplib import SMTPAuthenticationError, SMTPConnectError, SMTPException
-
-from django.db import DatabaseError
-from django.template.exceptions import TemplateDoesNotExist, TemplateSyntaxError
 from django.template.loader import render_to_string
 
-from authentication.exceptions import MailingServiceFailure
-from authentication.services.mailing.base import dispatch_mail
+from authentication.services.mailing.base import dispatch_mail, handle_mailing_errors
 from authentication.services.tokens import generate_code, save_code
 from authentication.utils import format_expiry, generate_plain_text_from_html
-
-logger = logging.getLogger(__name__)
 
 
 def send_code(email: str, code: str, expiry: int | float) -> None:
     expiry_display = format_expiry(expiry)
-
     subject = "Confirmation code to log in your account"
     html_content = render_to_string(
         "emails/verification_code.html",
@@ -34,34 +25,14 @@ def send_code(email: str, code: str, expiry: int | float) -> None:
     )
 
 
-def generate_otp(email, expiry: int | float) -> None:
-    try:
+def generate_otp(email: str, expiry: int | float) -> None:
+    with handle_mailing_errors():
         # The newest code is used for validation
         code, hashed_code = generate_code()
         save_code(
             hashed_code, email, expiry
         )  # Automatically invalidate previous unused codes
         send_code(email, code, expiry)
-    except (TemplateDoesNotExist, TemplateSyntaxError) as template_error:
-        # Your template is missing or broken
-        logger.error(f"Template error: {template_error}")
-        raise MailingServiceFailure
-    except SMTPAuthenticationError as auth_error:
-        # Email credentials are wrong
-        logger.error(f"Authentication error: {auth_error}")
-        raise MailingServiceFailure
-    except SMTPConnectError as connect_error:
-        # Can't reach the mail server
-        logger.error(f"Connection error: {connect_error}")
-        raise MailingServiceFailure
-    except SMTPException as unexpected_smtp_error:
-        # Catch-all for any other SMTP failure
-        logger.error(f"Unexpected error: {unexpected_smtp_error}")
-        raise MailingServiceFailure
-    except DatabaseError as db_error:
-        # Token failed to save
-        logger.exception(f"Database error: {db_error}")  # Trace failed queries
-        raise MailingServiceFailure
 
 
 def send_otp_email(email: str) -> None:
